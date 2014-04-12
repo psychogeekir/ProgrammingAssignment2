5ca12fff6d0b95aeb7972523c63b7c871f7f8d97
psychogeekir <zql0419@gmail.com>
2014-04-12 15:25:15 +0800

    R_Programming_Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creating a special "vector", which is a list containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
+## ivm indicates the inverse of the matrix
+## exivm indicates ivm which already exists
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ivm <- NULL
+        set <- function(y) {
+                x <<- y
+                ivm <<- NULL
+        }
+        get <- function() x
+        setivm <- function(exivm) ivm <<- exivm
+        getivm <- function() ivm
+        list(set = set, get = get,
+             setivm = setivm,
+             getivm = getivm)
 }
 
-
-## Write a short comment describing this function
+## calculating the inverse of the matrix based on the above function. It first checks to see if the ivm has already been calculated. If so, it gets the ivm from the cache and skips the computation. Otherwise, it calculates the ivm of the data and sets the value of the ivm in the cache via the setivm function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ivm <- x$getivm()
+        if(!is.null(ivm)) {
+                message("getting cached data")
+                return(ivm)
+        }
+        data <- x$get()
+        ivm <- solve(data, ...)
+        x$setivm(ivm)
+        ivm
 }
